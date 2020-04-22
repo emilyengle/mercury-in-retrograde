d8cfffdab5adbfed7a6fbe139872d3e9e53c6a30
Emily Engle <emily.engle.97@gmail.com>
2020-04-22 18:01:30 -0400

    Is mercury in retrograde?

--- index.js
+++ index.js
@@ -3,63 +3,46 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const { WebhookClient } = require('dialogflow-fulfillment');
-const { Card, Suggestion } = require('dialogflow-fulfillment');
+const { dialogflow, BasicCard, Image } = require('actions-on-google');
+const fetch = require('isomorphic-fetch');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
-  (request, response) => {
-    const agent = new WebhookClient({ request, response });
-    console.log(
-      'Dialogflow Request headers: ' + JSON.stringify(request.headers)
-    );
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+const URL = 'https://mercuryretrogradeapi.com';
+const BACKGROUND_IMAGE =
+  'https://cdn.dribbble.com/users/940359/screenshots/4129769/mercury.jpg';
 
-    function welcome(agent) {
-      agent.add(`Welcome to my agent!`);
-    }
+const app = dialogflow({ debug: true });
 
-    function fallback(agent) {
-      agent.add(`I didn't understand`);
-      agent.add(`I'm sorry, can you try again?`);
-    }
+app.intent('Is mercury in retrograde?', (conv) => {
+  // Note: Moving this fetch call outside of the app intent callback will
+  // cause it to become a global var (i.e. it's value will be cached across
+  // function executions).
+  return fetch(URL)
+    .then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error(response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      const message = data.is_retrograde
+        ? 'Mercury is in retrograde.'
+        : "Mercury is not in retrograde. It's your fault.";
+      conv.close(message);
+      if (conv.screen) {
+        conv.close(
+          new BasicCard({
+            text: message,
+            image: new Image({
+              url: BACKGROUND_IMAGE,
+              alt: 'Image of Mercury in retrograde',
+            }),
+          })
+        );
+      }
+    });
+});
 
-    // // Uncomment and edit to make your own intent handler
-    // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-    // // below to get this function to be run when a Dialogflow intent is matched
-    // function yourFunctionHandler(agent) {
-    //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-    //   agent.add(new Card({
-    //       title: `Title: this is a card title`,
-    //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-    //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-    //       buttonText: 'This is a button',
-    //       buttonUrl: 'https://assistant.google.com/'
-    //     })
-    //   );
-    //   agent.add(new Suggestion(`Quick Reply`));
-    //   agent.add(new Suggestion(`Suggestion`));
-    //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-    // }
-
-    // // Uncomment and edit to make your own Google Assistant intent handler
-    // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-    // // below to get this function to be run when a Dialogflow intent is matched
-    // function googleAssistantHandler(agent) {
-    //   let conv = agent.conv(); // Get Actions on Google library conv instance
-    //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-    //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-    // }
-    // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-    // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
-
-    // Run the proper function handler based on the matched Dialogflow intent name
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-    // intentMap.set('your intent name here', yourFunctionHandler);
-    // intentMap.set('your intent name here', googleAssistantHandler);
-    agent.handleRequest(intentMap);
-  }
-);
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
