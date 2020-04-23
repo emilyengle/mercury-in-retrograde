1ab984d4923ba4ab57be98261d70b0d4477194ee
Emily Engle <emily.engle.97@gmail.com>
2020-04-23 19:38:53 -0400

    Move into folders

--- getRetrogradeStatus/index.js
+++ getRetrogradeStatus/index.js
@@ -8,17 +8,14 @@ const fetch = require('isomorphic-fetch');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-const URL = 'https://mercuryretrogradeapi.com';
 const BACKGROUND_IMAGE =
   'https://cdn.dribbble.com/users/940359/screenshots/4129769/mercury.jpg';
 
 const app = dialogflow({ debug: true });
 
-app.intent('Is mercury in retrograde?', (conv) => {
-  // Note: Moving this fetch call outside of the app intent callback will
-  // cause it to become a global var (i.e. it's value will be cached across
-  // function executions).
-  return fetch(URL)
+const getRetrogradeStatus = (conv) => {
+  console.log('Fetching retrograde status.');
+  return fetch('https://mercuryretrogradeapi.com')
     .then((response) => {
       if (response.status < 200 || response.status >= 300) {
         throw new Error(response.statusText);
@@ -43,6 +40,31 @@ app.intent('Is mercury in retrograde?', (conv) => {
         );
       }
     });
-});
+};
+
+const getNextRetrogradeDate = (conv) => {
+  console.log('Fetching next retrograde date.');
+  return fetch('https://www.ismercuryinretrograde.com/')
+    .then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error(response.statusText);
+      } else {
+        console.log('raw response', response);
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log('returned from page: ', data);
+      conv.close('Working on it');
+    });
+};
+
+app.intent('Is mercury in retrograde?', getRetrogradeStatus);
+
+app.intent('When is mercury in retrograde next?', getNextRetrogradeDate);
+app.intent('When is it next?', getNextRetrogradeDate);
+app.intent('When is mercury next in retrograde?', getNextRetrogradeDate);
+app.intent('When is next?', getNextRetrogradeDate);
+app.intent('When will it be in retrograde?', getNextRetrogradeDate);
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
