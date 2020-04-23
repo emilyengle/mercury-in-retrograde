e1fc1c4fd6ae50c7a0a206264ba1ac84461ead30
Emily Engle <emily.engle.97@gmail.com>
2020-04-23 19:41:53 -0400

    Add getNextRetrogradeDate

--- getRetrogradeStatus/index.js
+++ getRetrogradeStatus/index.js
@@ -42,29 +42,6 @@ const getRetrogradeStatus = (conv) => {
     });
 };
 
-const getNextRetrogradeDate = (conv) => {
-  console.log('Fetching next retrograde date.');
-  return fetch('https://www.ismercuryinretrograde.com/')
-    .then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(response.statusText);
-      } else {
-        console.log('raw response', response);
-        return response.json();
-      }
-    })
-    .then((data) => {
-      console.log('returned from page: ', data);
-      conv.close('Working on it');
-    });
-};
-
 app.intent('Is mercury in retrograde?', getRetrogradeStatus);
-
-app.intent('When is mercury in retrograde next?', getNextRetrogradeDate);
-app.intent('When is it next?', getNextRetrogradeDate);
-app.intent('When is mercury next in retrograde?', getNextRetrogradeDate);
-app.intent('When is next?', getNextRetrogradeDate);
-app.intent('When will it be in retrograde?', getNextRetrogradeDate);
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
